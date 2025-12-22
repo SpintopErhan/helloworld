f3ab705510db59a25fa4928b2c5fc9c866b7c76a
SpintopErhan <karadaggames@gmail.com>
2025-12-22 16:39:27 +0300

    Base update

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
+// next.config.mjs
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true, // Genellikle önerilir, isteğe bağlı
+  transpilePackages: ['wagmi', 'viem', '@wagmi/connectors', '@tanstack/react-query'], // Bu satırı ekleyin!
+};
 
 export default nextConfig;
